791bd6a5c3f46048f63c48e0ad4fab5e32136ce2
Emman <manugasemmanuell@gmail.com>
2023-07-21 11:24:34 +0800

    add new git tech

--- src/assets/images/techdata.jsx
+++ src/assets/images/techdata.jsx
@@ -3,6 +3,7 @@ const Tech = [
     {src: require('../../assets/images/css.png')},
     {src: require('../../assets/images/javascript.png')},
     {src: require('../../assets/images/reactjs.png')},
+    {src: require('../../assets/images/git.png')},
     {src: require('../../assets/images/python.png')},
     {src: require('../../assets/images/mysql.png')},
     {src: require('../../assets/images/cplus.png')},
